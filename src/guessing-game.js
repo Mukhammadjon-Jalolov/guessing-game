dbdbee58341feb4764001267541e54d21341996a
Muhammadjon <mjalolov1305@gmail.com>
2022-08-14 12:44:26 +0500

    completed the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max;
     }
 
     guess() {
-
+        this.initial = Math.floor((this.max + this.min)/2);
+        return this.initial;
     }
 
     lower() {
-
+        this.max = this.initial;
     }
 
     greater() {
-
+        this.min = this.initial + 1;
     }
 }
 
